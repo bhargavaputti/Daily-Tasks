7c1c097ab96a72c7e0be27cf3c1871c930bddc37
bhargavaputti <bhargav.putti@encora.com>
2025-10-08 10:57:41 +0530

    Add files via upload
    This project demonstrates working of producer and consumer service with kafka server

---
+++ 07-10-2025/RetailProject/InventoryService33/src/main/java/com/inv/Inventory.java
@@ -0,0 +1,11 @@
+package com.inv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Inventory {
+	public static void main(String[] args) {
+		SpringApplication.run(Inventory.class, args);
+	}
+}
