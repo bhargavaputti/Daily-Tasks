cc5207c4b7573cbc1ea74b39241f151b9a0818d5
bhargavaputti <bhargav.putti@encora.com>
2025-10-08 18:29:26 +0530

    Add files via upload

---
+++ 08-10-2025/producer/src/test/java/org/encora/producer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.encora.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
